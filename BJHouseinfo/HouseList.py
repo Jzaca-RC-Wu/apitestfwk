70be944833940da9f2e730cc54d7331a8b86408f
Jzaca-RC-Wu <545653435@qq.com>
2018-10-15 12:37:04 +0800

    [feature] 获取搜索结果页总数

--- BJHouseinfo/HouseList.py
+++ BJHouseinfo/HouseList.py
@@ -4,6 +4,8 @@ from urllib.request import urlopen
 import requests
 
 HouseListUrl = "http://www.gzbjfc.com/House/HouseList.aspx"
+HouseListUrl_page = "http://www.gzbjfc.com/House/HouseList.aspx?page={}"
+
 
 
 class HouseList:
@@ -11,18 +13,39 @@ class HouseList:
     def __init__(self, url):
         self.url = url
 
-    def get_xml(self):
+    def get_html(self):
         res = sdq(self.url).geturlresp()
         tree = html.parse(res)
         # print(etree.tostring(tree))
         r = tree.xpath("//table[@class=\"Repeater\"]")
-        print(len(r))
+        # print(len(r))
         return tree
 
-    def get_house_list(self):
+    def get_search_pages(self):
         # Todo
-        # for
-        pass
+        tree = self.get_html()
+        result = tree.xpath("//div[@id=\"cph_hl1_pagerTop\"]/a/@href")
+        pages_nums = result[-1].split("=")[1]
+        print(pages_nums)
 
-resp = HouseList(HouseListUrl).get_xml()
+        print(result)
+        print(len(result))
+        # print(result[1].text)
+        return pages_nums
+
+    def get_house_pros_info(self):
+        tree = self.get_html()
+        result = tree.xpath('//table[@class=\"Repeater\"]')
+        print(type(result[0]))
+
+        house_pros = []
+        for i in range(len(result)):
+            house = {}
+            house["houseimg"] = result[i].xpath['//div[@class="houseImg"]']
+            house["housename"] = result[i].xpath['//a[@class="url"]']
+            house["pre_num"] = result[i].xpath['//table[@]']
+            print(house)
+
+# resp = HouseList(HouseListUrl).get_html()
+print(HouseList(HouseListUrl).get_house_pros_info())
 # print(resp)
